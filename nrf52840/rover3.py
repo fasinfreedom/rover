7ea2cd1a5017926a7d3d1d1f8eb0d9ab02d8ac33
fasinfreedom <pranatic@proton.me>
2024-06-23 14:21:38 -0500

    PWM working, pixel reacts to close distance

--- nrf52840/rover3.py
+++ nrf52840/rover3.py
@@ -4,27 +4,20 @@ import os
 from hcsr04 import HCSR04
 import neopixel
 
-#Motor 1
-#GP11 - PWMA Speed Control
-#GP14 - AIN1 Direction Control
-#GP15 - AIN2 Direction Control
+maxSpeed = 0xFFFF
 
-#Motor 2
-#GP10 - PWMB Speed Control
-#GP13 - BIN1 Direction Control
-#GP12 - BIN2 Direction Control
-
-maxSpeed = 2 ** 16 - 1
-
+# Classes
+# ========================================================================
 class Motor:
     name = None
     in1 = None
     in2 = None
     pwm = None
+    speed = maxSpeed
 
     def __init__(self, name, in1, in2, pwm):
         self.name = name
-        self.pwm = pwmio.PWMOut(pwm, duty_cycle = maxSpeed, frequency = 1500)
+        self.pwm = pwmio.PWMOut(pwm, duty_cycle=self.speed, frequency=1000)
         self.in1 = digitalio.DigitalInOut(in1)
         self.in1.direction = digitalio.Direction.OUTPUT
         self.in2 = digitalio.DigitalInOut(in2)
@@ -46,78 +39,36 @@ class Motor:
         self.in1.value = False
         self.in2.value = False
 
-    def speed(self, speed):
-        self.pwm.duty_cycle = speed
+    def setSpeed(self, speed):
+        self.speed = speed if speed >= 25 else 25
+        self.pwm.duty_cycle = round(maxSpeed * self.speed / 100)
 
     def on(self):
         self.forward()
-        self.speed(maxSpeed)
 
     def print(self):
         print(f"\nMotor: {self.name}")
-        print(f"PWM: {self.pwm.duty_cycle/65535*100}%")
+        print(f"Speed: {self.speed}%")
         print(f"In1: {self.in1.value}")
         print(f"In2: {self.in2.value}")
 
+
+# Functions
+# ========================================================================
 def pixelOn(pixel):
-    pixel.brightness = 0.2
+    pixel.brightness = 0.1
+
 
 def pixelOff(pixel):
     pixel.brightness = 0
 
-pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
-pixel.fill((50, 0, 255))
-pixelOff(pixel)
-
-button = digitalio.DigitalInOut(board.D2)
-button.direction = digitalio.Direction.INPUT
-button.pull = digitalio.Pull.DOWN
-
-# motorStandby being False causes motors to be off
-# motorStandby = digitalio.DigitalInOut(board.GP9)
-# motorStandby.direction = digitalio.Direction.OUTPUT
-
-motors = []
-motor1 = Motor("Motor1", board.D9, board.D10, board.A5)
-motors.append(motor1)
-motor2 = Motor("Motor2", board.D11, board.D12, board.A4)
-motors.append(motor2)
-
-systemsOn = False
-# motorStandby = True
-
-print("\nCOMMENCING PROGRAM")
-
-ultrasonicTrig = board.A1
-ultrasonicEcho = board.A0
-
-systemStatusCurrent = False
-sonar = HCSR04(ultrasonicTrig, ultrasonicEcho)
-
-while True:
-    if systemsOn:
-        try:
-            ultrasonicDistance = sonar.dist_cm()
-            print(f"Distance: {ultrasonicDistance}")
-            if ultrasonicDistance < 10:
-                print("Close Object Detected")
-        except KeyboardInterrupt:
-            pass
-
-    if systemStatusCurrent != button.value:
-        print(f"\nSystem powering {'On' if button.value else 'Off'}")
-        systemsOn = button.value
-        systemStatusCurrent = systemsOn
-        pixelOn(pixel) if systemsOn else pixelOff(pixel)
-        for motor in motors:
-            motor.on() if systemsOn else motor.off()
-        print(f"Systems {'On' if systemsOn else 'Off'}")
-
-        for motor in motors:
-            motor.print()
-
-    time.sleep(.1)
-
+def pixelColor(pixel, color):
+    if color == 'ORANGE':
+        pixel.fill((255, 50, 0))
+        return
+    if color == 'RED':
+        pixel.fill((255, 0, 0))
+        return
 
 def turnLeft(motors):
     motors[0].off()
@@ -129,3 +80,69 @@ def turnRight(motors):
 
 def newRoute(motors):
     turnLeft(motors)
+
+
+# Main Program
+# ========================================================================
+pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
+pixel.fill((255, 50, 0))
+pixelOff(pixel)
+
+button = digitalio.DigitalInOut(board.D2)
+button.direction = digitalio.Direction.INPUT
+button.pull = digitalio.Pull.DOWN
+
+# motorsEnable set to false causes motor controller to go into Standby
+motorsEnable = digitalio.DigitalInOut(board.D11)
+motorsEnable.direction = digitalio.Direction.OUTPUT
+motorsEnable = True
+motorSpeed = 50 #25-100
+
+motors = []
+motor1 = Motor("Motor1", board.D5, board.D6, board.A5)
+motor1.setSpeed(motorSpeed)
+motors.append(motor1)
+motor2 = Motor("Motor2", board.D9, board.D10, board.A4)
+motor2.setSpeed(motorSpeed)
+motors.append(motor2)
+
+
+systemsOn = False
+
+print("\nCOMMENCING PROGRAM")
+
+sonarTrig = board.A1
+sonarEcho = board.A0
+
+systemStatusCurrent = False
+sonar = HCSR04(sonarTrig, sonarEcho)
+sonarDistance = 0
+
+while True:
+    if systemsOn:
+        try:
+            sonarDistanceNew = round(sonar.dist_cm())
+            if sonarDistance != sonarDistanceNew:
+                print(f"Distance: {sonarDistance} cm")
+                pixelColor(pixel, 'ORANGE')
+                if sonarDistance < 10:
+                    print("Close Object Detected")
+                    pixelColor(pixel, 'RED')
+            sonarDistance = sonarDistanceNew
+        except KeyboardInterrupt:
+            pass
+
+    if systemStatusCurrent != button.value:
+        systemsOn = button.value
+        print(f"\nSystem powering {'On' if button.value else 'Off'}")
+        systemStatusCurrent = systemsOn
+        pixelOn(pixel) if systemsOn else pixelOff(pixel)
+        for motor in motors:
+            motor.on() if systemsOn else motor.off()
+        print(f"Systems {'On' if systemsOn else 'Off'}")
+
+        print(f"Motors {'Enabled' if motorsEnable else 'Disable'}")
+        for motor in motors:
+            motor.print()
+
+    time.sleep(0.1)
